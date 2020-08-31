1509ed25b2e18d2d5f8b9f5de4b55d85231944f2
Sam Wells <wellssam100@gmail.com>
2020-08-30 20:17:37 -0400

    Finished Raycasting project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Vector3 dirControl = new Vector3 (0, 0, 0);
+    private Camera _Camera;
+    private float rotation = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)||Input.GetKeyDown(KeyCode.A)) 
+        {
+            dirControl += -(transform.right);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            dirControl += transform.right;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            dirControl += transform.forward;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            dirControl += -(transform.forward);
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
+        {
+            dirControl -= -(transform.right);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
+        {
+            dirControl -= transform.right;
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
+        {
+            dirControl -= transform.forward;
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+        {
+            dirControl -= -(transform.forward);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            rotation += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            rotation += -1;
+        }
+        if (Input.GetKeyUp(KeyCode.E)) 
+        {
+            rotation -= 1;
+        }
+        if (Input.GetKeyUp(KeyCode.Q)) 
+        {
+            rotation -= -1;
+        }
+
+
+
+        if (!dirControl.Equals(Vector3.zero))
+        transform.position += dirControl;
+
+        transform.Rotate(0.0f, rotation, 0.0f, Space.Self);
+        
+    }
+    
+    
+    
+}
---
+++ Assets/Scripts/RayTracing.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class RayTracing : MonoBehaviour
+{
+    //SETUP DISPLAY
+    public ComputeShader RayTracingShader;
+    private RenderTexture _target;
+    //SETUP CAMERA
+    private Camera _camera;
+    //SETUP SKYBOX
+    public Texture SkyboxTexture;
+    //Anti Aliasing Samples
+    private uint _currentSample = 0;
+    private Material _addMaterial;
+    //Directional Lighting
+    public Light DirectionalLight;
+
+    //Public Options
+    public Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
+    public uint SpheresMax = 100;
+    public float SpherePlacementRadius = 50.0f;
+    private ComputeBuffer _sphereBuffer;
+
+    public struct Sphere
+    {
+        public Vector3 position;
+        public float radius;
+        public Vector3 albedo;
+        public Vector3 specular;
+    }
+
+    //SETUP DISPLAY
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        SetShaderParameters();
+        Render(destination);
+    }
+
+    private void OnEnable() 
+    {
+        _currentSample = 0;
+        SetUpScene();
+    }
+
+    private void OnDisable() 
+    {
+        if (_sphereBuffer != null)
+            _sphereBuffer.Release();
+    }
+
+    private void SetUpScene()
+    {
+        List<Sphere> spheres = new List<Sphere>();
+
+        //Add a number of spheres to the list
+        for (int i = 0; i < SpheresMax; i++) 
+        {
+            Sphere sphere = new Sphere();
+
+            sphere.radius = SphereRadius.x + UnityEngine.Random.value * (SphereRadius.y - SphereRadius.x);
+            Vector2 randomPos = UnityEngine.Random.insideUnitCircle * SpherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+
+            //reject spheres that are intersecting others
+            foreach (Sphere other in spheres)
+            {
+                float minDist = sphere.radius + other.radius;
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist) 
+                {
+                    goto SkipSphere;
+                }
+            }
+
+            Color color = UnityEngine.Random.ColorHSV();
+            bool metal = UnityEngine.Random.value < 0.5f;
+            sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
+            sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.04f;
+
+            spheres.Add(sphere);
+
+        SkipSphere:
+            continue;
+
+        }
+
+        //Assign to compute buffer
+        _sphereBuffer = new ComputeBuffer(spheres.Count, 40);
+        _sphereBuffer.SetData(spheres);
+    }
+
+    private void Awake() 
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void Update() 
+    {
+        //To restart the anti-aliasing sampling
+        if (transform.hasChanged) 
+        {
+            _currentSample = 0;
+            transform.hasChanged = false;
+        }
+        if (DirectionalLight.transform.hasChanged) 
+        {
+            _currentSample = 0;
+            DirectionalLight.transform.hasChanged = false;
+        }
+
+    }
+
+    //SETUP DISPLAY
+    private void Render (RenderTexture destination)
+    {
+        InitRenderTexture();
+
+        //set the target and dispatch the compute shader
+        RayTracingShader.SetTexture(0, "Result", _target);
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+
+        if (_addMaterial == null) 
+        {
+            _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
+        }
+        _addMaterial.SetFloat("_Sample", _currentSample);
+
+        //Blit the Results
+        //Blit(source,dest):Copies source texture into destination render texture with a shader.
+        //Blit(source, dest, mat ) mat == Material to use. Material's shader could do some post-processing effect, for example.
+        Graphics.Blit(_target, destination,_addMaterial);
+        _currentSample++;
+    }
+   
+    //SETUP DISPLAY
+    private void InitRenderTexture() 
+    {
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
+        {
+            //Release render texture if we already have one
+            if (_target != null)
+            {
+                _target.Release();
+            }
+
+            _target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
+
+
+    }
+
+    //SETUP CAMERA
+    private void SetShaderParameters() 
+    {
+        Vector3 l = DirectionalLight.transform.forward;
+        RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
+        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
+
+        //Set the texture on the shader
+        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
+        //Anti-Aliasing pixel offset
+        RayTracingShader.SetVector("_PixelOffset", new Vector2(UnityEngine.Random.value, UnityEngine.Random.value));
+        //Directional Lighting direction
+        RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+
+        RayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
+    }
+
+    
+}
