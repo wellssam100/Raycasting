449a603dce681e39858ceee76d5d28da5c420635
Sam Wells <wellssam100@gmail.com>
2020-09-06 12:58:30 -0400

    Split up ford sphere code into seperate functions

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -22,37 +22,37 @@ public class CameraController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow)||Input.GetKeyDown(KeyCode.A)) 
         {
-            dirControl += -(transform.right);
+            dirControl += -(transform.right)/5;
         }
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
-            dirControl += transform.right;
+            dirControl += transform.right / 5;
         }
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
-            dirControl += transform.forward;
+            dirControl += transform.forward / 5;
         }
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
         {
-            dirControl += -(transform.forward);
+            dirControl += -(transform.forward) / 5;
         }
 
 
         if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
         {
-            dirControl -= -(transform.right);
+            dirControl -= -(transform.right) / 5;
         }
         if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
         {
-            dirControl -= transform.right;
+            dirControl -= transform.right / 5;
         }
         if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
         {
-            dirControl -= transform.forward;
+            dirControl -= transform.forward / 5;
         }
         if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
         {
-            dirControl -= -(transform.forward);
+            dirControl -= -(transform.forward) / 5;
         }
 
         if (Input.GetKeyDown(KeyCode.E)) 
--- Assets/Scripts/RayTracing.cs
+++ Assets/Scripts/RayTracing.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -25,12 +26,21 @@ public class RayTracing : MonoBehaviour
     public Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
     public int SpheresMax = 100;
     public float SpherePlacementRadius = 50.0f;
-    private ComputeBuffer _sphereBuffer;
+    private ComputeBuffer _fordSphereBuffer, _randomSphereBuffer;
+
+    public Color myColor;
 
     private int SpheresTotal;
+    //Sphere Bob variable
     private float step;
+    //Ford's Sphere tree depth
+    public int depth;
 
-
+    public struct Pair 
+    {
+        public int a;
+        public int b;
+    }
     public struct Sphere
     {
         public Vector3 position;
@@ -54,16 +64,35 @@ public class RayTracing : MonoBehaviour
 
     private void OnDisable() 
     {
-        if (_sphereBuffer != null)
-            _sphereBuffer.Release();
+        if (_fordSphereBuffer != null)
+            _fordSphereBuffer.Release();
     }
 
     private void SetUpScene()
     {
-        List<Sphere> spheres = new List<Sphere>();
+        List<Sphere> fordSpheres = new List<Sphere>();
+        List<Sphere> randomSpheres = new List<Sphere>();
+        List<Pair> fractionMap =  new List<Pair>();
+        
 
+        drawRandomSpheres(randomSpheres);
+        setupFordSpheres(fordSpheres, fractionMap);
+               
+        //Assign Ford Spheres to compute buffer
+        _fordSphereBuffer = new ComputeBuffer(fordSpheres.Count, 40);
+        _fordSphereBuffer.SetData(fordSpheres);
+        SpheresTotal = fordSpheres.Count;
+
+
+
+       
+        
+    }
+
+    private void drawRandomSpheres(List<Sphere> spheres) 
+    {
         //Add a number of spheres to the list
-        for (int i = 0; i < SpheresMax; i++) 
+        for (int i = 0; i < SpheresMax; i++)
         {
             Sphere sphere = new Sphere();
 
@@ -75,12 +104,12 @@ public class RayTracing : MonoBehaviour
             foreach (Sphere other in spheres)
             {
                 float minDist = sphere.radius + other.radius;
-                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist) 
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist)
                 {
                     goto SkipSphere;
                 }
             }
-
+            
             Color color = UnityEngine.Random.ColorHSV();
             bool metal = UnityEngine.Random.value < 0.5f;
             sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
@@ -92,14 +121,74 @@ public class RayTracing : MonoBehaviour
             continue;
 
         }
+    }
+    private void setupFordSpheres(List<Sphere> spheres, List<Pair> fractionMap) 
+    {
+        Sphere s1, s2 = new Sphere();
+        s1.position = new Vector3(0, 0.5f, 0);
+        s2.position = new Vector3(1, 0.5f, 0);
+        s1.radius = 0.5f;
+        s2.radius = 0.5f;
 
-        //Assign to compute buffer
-        _sphereBuffer = new ComputeBuffer(spheres.Count, 40);
-        _sphereBuffer.SetData(spheres);
-        SpheresTotal = spheres.Count;
+        Color colorBase_1 = new Color(s1.position.x, s1.position.x, s1.position.x);
+        Color colorBase_2 = new Color(s2.position.x, s2.position.x, s2.position.x);
 
-       
-        
+        Color color_1 = colorBase_1 * myColor;
+        Color color_2 = colorBase_2 * myColor;
+
+
+        bool metal = UnityEngine.Random.value < 0.5f;
+        s1.albedo = Vector3.zero;
+        s1.specular = new Vector3(color_1.r, color_1.g, color_1.b);
+        s2.albedo = Vector3.zero;
+        s2.specular = new Vector3(color_2.r, color_2.g, color_2.b);
+
+
+        spheres.Add(s1);
+        spheres.Add(s2);
+
+        drawFordSpheres(1, 2, depth, spheres, fractionMap);
+    }
+    private void drawFordSpheres(int a, int b, int step , List<Sphere> spheres, List<Pair> fractionMap) 
+    {
+        if (step == 0) 
+        {
+            return;
+        }
+        //create hashmap with fractions in it (or maybe key-values of a,b)
+        Pair p = new Pair();
+        p.a = a;
+        p.b = b;
+
+        if (!fractionMap.Contains(p)) 
+        {
+            //Set values
+            Sphere sphere = new Sphere();
+            sphere.position = new Vector3 ((float)a / (float)b, 1.0f / (2.0f * (float)(Math.Pow(b, 2))), 0);
+            sphere.radius = 1.0f / (2.0f * (float)(Math.Pow(b, 2)));
+            //Add Gradient Color
+            Color colorBase = new Color(sphere.position.x, sphere.position.x, sphere.position.x);
+            Color color = colorBase * myColor;
+            sphere.albedo = new Vector3(color.r, color.g, color.b);
+            sphere.specular = Vector3.one * 0.04f;
+            //Add Sphere to Lst
+            spheres.Add(sphere);
+        }
+        step--;
+        fractionMap.Add(p);
+       // UnityEngine.Debug.Log("x: " + p.a + "/" + p.b + "\n");
+        drawFordSpheres(a, b + 1, step, spheres, fractionMap);
+        drawFordSpheres(a + 1, b + 1, step, spheres, fractionMap);
+    
+    }
+    //returns true if the elements are the same, false if they are different
+    private bool comPair(Pair p1, Pair p2) 
+    {
+        if (p1.a == p2.a && p1.b == p2.b) 
+        {
+            return true;
+        }
+        return false;
     }
 
     private void Awake() 
@@ -122,7 +211,7 @@ public class RayTracing : MonoBehaviour
             _currentSample = 0;
             DirectionalLight.transform.hasChanged = false;
         }
-       // SphereBob();
+        //SphereBob();
 
 
 
@@ -134,7 +223,7 @@ public class RayTracing : MonoBehaviour
         List<Sphere> spheres = new List<Sphere>();
 
         Sphere[] tempArray = new Sphere[SpheresTotal];
-        _sphereBuffer.GetData(tempArray, 0, 0, SpheresTotal);
+        _randomSphereBuffer.GetData(tempArray, 0, 0, SpheresTotal);
         for (int i = 0; i < tempArray.Length; i++) 
         {
             spheres.Add(tempArray[i]);
@@ -147,19 +236,30 @@ public class RayTracing : MonoBehaviour
         foreach(Sphere sphere in spheres)
         {
             Sphere tempSphere = sphere;
-            float up = (float)(Math.Sin(step));
-            step = step + (float)Math.PI / 72;
+            float up = (float)(Math.Sin(step))/ sphere.radius;
+            step = (step + (float)Math.PI / 2048)%((float)(Math.PI)*2);
             tempSphere.position.y = sphere.position.y + up;
             temp.Add(tempSphere);
         }
-        _sphereBuffer.SetData(temp);
+        _randomSphereBuffer.SetData(temp);
         Sphere[] check = new Sphere[1];
-        _sphereBuffer.GetData(check, 0, 0, 1); 
+        _randomSphereBuffer.GetData(check, 0, 0, 1); 
         _currentSample = 0;
 
-        RayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
+        RayTracingShader.SetBuffer(0, "_Spheres", _randomSphereBuffer);
 
     }
+
+    //to each rational number a/b (in lowest terms), assign a circle above but tangent to the x-axis at a/b with radius1/2b2
+    /*private void fordLine(List<Sphere> spheres) 
+    {
+        for(int i=0;i<SpheresMax;i++)
+        {
+            spheres[i].radius = ((float)(1)) / (2 * Math.Pow(SpheresMax, 2));
+            spheres[i].position = (i / SpheresMax,sphere.radius,0);
+        }
+    }*/
+   
 
     //SETUP DISPLAY
     private void Render (RenderTexture destination)
@@ -218,8 +318,8 @@ public class RayTracing : MonoBehaviour
         RayTracingShader.SetVector("_PixelOffset", new Vector2(UnityEngine.Random.value, UnityEngine.Random.value));
         //Directional Lighting direction
         RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
-
-        RayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
+        //TODO: figure out if I can set then draw from two different buffers
+        RayTracingShader.SetBuffer(0, "_Spheres", _fordSphereBuffer);
     }
 
     
